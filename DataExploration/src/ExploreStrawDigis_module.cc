d8862bb1426b5afef9d4f2120ffd8af41be25893
Rob Kutschke <kutschke@fnal.gov>
2023-08-19 14:57:46 -0500

    Update the explore straw digis code and it's fcl.

--- DataExploration/src/ExploreStrawDigis_module.cc
+++ DataExploration/src/ExploreStrawDigis_module.cc
@@ -2,19 +2,17 @@
 //  ExploreStrawDigis module.  Examine features of StrawDigi
 //
 
-// C++ includes.
-#include <iostream>
+#include "Offline/RecoDataProducts/inc/StrawDigi.hh"
+#include "Offline/RecoDataProducts/inc/ComboHit.hh"
 
-// Framework includes
 #include "art/Framework/Core/EDAnalyzer.h"
 #include "art/Framework/Core/ModuleMacros.h"
 #include "art/Framework/Principal/Event.h"
 #include "art_root_io/TFileService.h"
-// Root includes
+
 #include "TH1F.h"
-// Mu2e Data Products
-#include "Offline/RecoDataProducts/inc/StrawDigi.hh"
-#include "Offline/RecoDataProducts/inc/ComboHit.hh"
+
+#include <iostream>
 
 namespace mu2e {
 
@@ -24,42 +22,48 @@ namespace mu2e {
     struct Config {
       using Name=fhicl::Name;
       using Comment=fhicl::Comment;
-      fhicl::Atom<art::InputTag> SDCTag{Name("StrawDigis"), Comment("StrawDigiCollection"), art::InputTag()};
+      fhicl::Atom<art::InputTag> SDCTag{Name("StrawDigisTag"), Comment("Input tag for a StrawDigiCollection")};
     };
     typedef art::EDAnalyzer::Table<Config> Parameters;
 
     explicit ExploreStrawDigis(const Parameters& conf);
 
-    void analyze(const art::Event& event);
-    void beginJob( );
+    void beginJob( ) override;
+    void analyze(const art::Event& event) override;
 
   private:
-    Config _conf;
-    TH1F *_nstrawdigi, *_tdc, *_adc, *_tot, *_deltatdc;
+    art::ProductToken<StrawDigiCollection> const _strawDigisToken;
+
+    TH1F* _nstrawdigi = nullptr;
+    TH1F* _tdc        = nullptr;
+    TH1F* _adc        = nullptr;
+    TH1F* _tot        = nullptr;
+    TH1F* _deltatdc   = nullptr;
 
   };
 
   ExploreStrawDigis::ExploreStrawDigis(const Parameters& conf)
     : art::EDAnalyzer(conf),
-      _conf(conf()) {
-    consumes<StrawDigiCollection>(_conf.SDCTag());
+      _strawDigisToken{consumes<StrawDigiCollection>(conf().SDCTag())}
+  {
   }
 
   void ExploreStrawDigis::beginJob( ){
     art::ServiceHandle<art::TFileService> tfs;
-      _nstrawdigi = tfs->make<TH1F>("NStrawDigis","N StrawDigis",250,0,5000);
-      _tdc = tfs->make<TH1F>("tdc","StrawDigis TDC",100,0,100000); 
-      _deltatdc = tfs->make<TH1F>("deltatdc","StrawDigis deltatdc",100,-100,1000);
-      _tot = tfs->make<TH1F>("tot","StrawDigis TOT",100,0,100);
-      _adc = tfs->make<TH1F>("adc","StrawDigis ADC",100,0,5000);
-
+    _nstrawdigi = tfs->make<TH1F>("NStrawDigis", "N StrawDigis",        250,    0.,  5000.);
+    _tdc        = tfs->make<TH1F>("tdc",         "StrawDigis TDC",      100,    0.,100000.);
+    _deltatdc   = tfs->make<TH1F>("deltatdc",    "StrawDigis deltatdc", 100, -100.,  1000.);
+    _tot        = tfs->make<TH1F>("tot",         "StrawDigis TOT",      100,    0.,   100.);
+    _adc        = tfs->make<TH1F>("adc",         "StrawDigis ADC",      100,    0.,  5000.);
   }
 
   void ExploreStrawDigis::analyze(const art::Event& event){
-  // find the StrawDigi collection in the event
-    auto sdcH = event.getValidHandle<StrawDigiCollection>(_conf.SDCTag());
-    auto const& sdc = *sdcH;
+
+    // Get the requestedd StrawDigiCollection
+    auto const& sdc = event.getProduct( _strawDigisToken );
+
     _nstrawdigi->Fill(sdc.size());
+
     // loop over strawdigis
     for(auto const& sd : sdc) {
       _tdc->Fill(sd.TDC(StrawEnd::cal));
@@ -71,5 +75,4 @@ namespace mu2e {
 
 } // end namespace mu2e
 
-using mu2e::ExploreStrawDigis;
-DEFINE_ART_MODULE(ExploreStrawDigis)
+DEFINE_ART_MODULE(mu2e::ExploreStrawDigis)
