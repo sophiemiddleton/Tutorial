294f60874c148c462f36aa4700299690d12dea4b
Andrew Edmonds <aedmonds@york.cuny.edu>
2024-10-27 16:59:12 -0500

    Delete old TrkAna tutorials

--- TrkAna/solutions/AdvEx04/src/DoubleWeight_module.cc
+++
@@ -1,7 +0,0 @@
-#include "Offline/TrkDiag/inc/WeightModule.hh"
-#include "inc/DoubleWeightPhys.hh"
-
-namespace mu2e {
-  typedef WeightModule<DoubleWeightPhys> DoubleWeight;
-}
-DEFINE_ART_MODULE(mu2e::DoubleWeight);
